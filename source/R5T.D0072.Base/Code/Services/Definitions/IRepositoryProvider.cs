7b880495283f6979fdfc33aa1baa084e9da1fa78
David Coats <david@getsafetycone.com>
2021-05-28 11:14:35 -0700

    Add Base service definition library and remove extraneous R5T.XXXX library.

---
+++ source/R5T.D0072.Base/Code/Services/Definitions/IRepositoryProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0072
+{
+    public interface IRepositoryProvider<TRepository>
+    {
+        Task<TRepository> GetRepository(string dataSourceName);
+    }
+}
